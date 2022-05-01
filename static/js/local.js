e9527f8614f80e872e0cb1c5e00e5305a838eeb9
xphreak5 <40873583+xphreak5@users.noreply.github.com>
2022-05-02 01:05:51 +0200

    Animations

--- static/js/local.js
+++ static/js/local.js
@@ -1,3 +1,34 @@
-// search butonu animasyon
-// jumbotron fadein effect
-// değişen yazılar jumbotron
+// Set h2 First
+$("#jumbotron").html("Hi Welcome")
+
+// Search Animation
+$("#search").click(function() {
+    $("#search").animate({
+        "padding-right": "90px"
+    }, 300);
+})
+
+$("#search").blur(function() {
+    $("#search").animate({
+        "padding-right": "20px"
+    }, 300);
+})
+
+
+// Jumbotron Effect
+var textArray = ["I am Boiz Bot", "Add me to your server", "I have some cool stuff", "Hi Welcome"]
+var counter = 0;
+function changeText() {
+    if (counter >= textArray.length) {
+        counter = 0;
+    }
+    
+    
+    $("#jumbotron").fadeOut(1000)
+    $("#jumbotron").html(textArray[counter])
+    $("#jumbotron").fadeOut(1000)
+    $("#jumbotron").fadeIn(3000)
+    counter++;
+}
+
+setInterval(changeText, 4000)
